86967dd7ecdacdb83b3574a89531c5cdba086756
zachp <zachp@DESKTOP-9B2ML8K>
2021-07-25 17:45:04 -0400

    Add project files.

---
+++ AllwynInPWA/Client/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllwynInPWA.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDc5MzI2QDMxMzkyZTMyMmUzMFMzOFhTWVBPMFJCMkpyTVRMbHIvOTJmZTU1MFZ2YXNxcmZTNi9UMEl5Ykk9");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSyncfusionBlazor();
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ AllwynInPWA/Shared/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllwynInPWA.Shared
+{
+    public class Job
+    {
+        public string JobTitle { get; set; }
+        public string JobDesc { get; set; }
+        public string EduLev { get; set; }
+        public int YearsExp { get; set; }
+    }
+}
